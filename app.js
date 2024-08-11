383d6cf256348acccd472a8d04957485f964a205
Rishab Prasad <rishabgt@gmail.com>
2024-08-11 12:39:55 +0530

    Episode 3 init

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello from Namaste React!"
-);
-const child = React.createElement("div", { id: "child" }, [heading, heading]);
-const parent = React.createElement("div", { id: "parent" }, child);
+//JSX transpiles before reaching the js engine - Parcel - Babel
+//JSX->ReactElement->HTML Element
+
+//React functional component
+
+const Title = () => <h1>This is a title component</h1>;
+
+const number = 3;
+
+//Component composition
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <h3>Lesson number {number}</h3>
+      <Title />
+      {Title()}
+      <h2 className="heading">A functional component header</h2>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
