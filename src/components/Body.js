6a0b5d73918d4ce6fa34658dd6e4e91ca921e302
Rishab <rishabgt@gmail.com>
2026-01-04 18:33:25 +0530

    useState hook added for filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import resData from "../utils/data";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resData);
+
   console.log(resData);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="btn-filter"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (items) => items.info.avgRating > 4.1
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resData.map((items, index) => (
-          <RestaurantCard key={index} resData={items} />//Using index as key for simplicity, 
-                                                    //but its a bad practice in real apps
+        {listOfRestaurants.map((items, index) => (
+          <RestaurantCard key={index} resData={items} /> //Using index as key for simplicity,
+          //but its a bad practice in real apps
         ))}
       </div>
     </div>
